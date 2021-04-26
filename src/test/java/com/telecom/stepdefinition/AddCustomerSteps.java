ecdbcb28acce9807607a1fbd1e1f2ab6d552e4eb
kathiS1007 <karthick@centricsoftware.com>
2021-04-26 19:58:58 +0530

    new one

--- src/test/java/com/telecom/stepdefinition/AddCustomerSteps.java
+++ src/test/java/com/telecom/stepdefinition/AddCustomerSteps.java
@@ -1,5 +1,8 @@
 package com.telecom.stepdefinition;
 
+import java.util.List;
+import java.util.Map;
+
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,6 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.cucumber.datatable.DataTable;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class AddCustomerSteps {
@@ -56,6 +60,62 @@ public class AddCustomerSteps {
 		Assert.assertTrue(driver.findElement(By.xpath("(//td[@align='center'])[2]")).isDisplayed());
 	    
 	}
+	
+
+   @When("user need to fill up the fields by one dim list")
+   public void user_need_to_fill_up_the_fields_by_one_dim_list(DataTable dataTable) {
+	   
+	   List<String> datas = dataTable.asList(String.class);
+	   driver.findElement(By.xpath("(//label[@for='done'])[1]")).click();
+		driver.findElement(By.id("fname")).sendKeys(datas.get(0));
+		driver.findElement(By.id("lname")).sendKeys(datas.get(1));
+		driver.findElement(By.id("email")).sendKeys(datas.get(2));
+		driver.findElement(By.name("addr")).sendKeys(datas.get(3));
+		driver.findElement(By.id("telephoneno")).sendKeys(datas.get(4));
+    
+  }
+   
+   
+   @When("user need to fill up the fields by one dim map")
+   public void user_need_to_fill_up_the_fields_by_one_dim_map(DataTable dataTable) {
+	   
+	   Map<String, String> datas = dataTable.asMap(String.class, String.class);
+        
+	    driver.findElement(By.xpath("(//label[@for='done'])[1]")).click();
+		driver.findElement(By.id("fname")).sendKeys(datas.get("fname"));
+		driver.findElement(By.id("lname")).sendKeys(datas.get("lname"));
+		driver.findElement(By.id("email")).sendKeys(datas.get("mail"));
+		driver.findElement(By.name("addr")).sendKeys(datas.get("addr"));
+		driver.findElement(By.id("telephoneno")).sendKeys(datas.get("phno"));
+    
+  }
+   
+   
+   @When("user need to fill up the fields by two dim list")
+   public void user_need_to_fill_up_the_fields_two_dim_list(DataTable dataTable) {
+	   
+	    List<List<String>> datas = dataTable.asLists(String.class);
+	   driver.findElement(By.xpath("(//label[@for='done'])[1]")).click();
+		driver.findElement(By.id("fname")).sendKeys(datas.get(1).get(0));
+		driver.findElement(By.id("lname")).sendKeys(datas.get(0).get(1));
+		driver.findElement(By.id("email")).sendKeys(datas.get(2).get(2));
+		driver.findElement(By.name("addr")).sendKeys(datas.get(0).get(3));
+		driver.findElement(By.id("telephoneno")).sendKeys(datas.get(1).get(4));
+    
+  }
+   
+   @When("user need to fill up the fields by two dim map")
+   public void user_need_to_fill_up_the_fields_two_dim_map(DataTable dataTable) {
+	   
+	     List<Map<String, String>> datas = dataTable.asMaps(String.class,String.class);
+	   driver.findElement(By.xpath("(//label[@for='done'])[1]")).click();
+		driver.findElement(By.id("fname")).sendKeys(datas.get(2).get("FN"));
+		driver.findElement(By.id("lname")).sendKeys(datas.get(0).get("LN"));
+		driver.findElement(By.id("email")).sendKeys(datas.get(2).get("ML"));
+		driver.findElement(By.name("addr")).sendKeys(datas.get(0).get("AD"));
+		driver.findElement(By.id("telephoneno")).sendKeys(datas.get(1).get("PH"));
+    
+  }
 
 
 }
--- src/test/resources/Features/AddCustomer.feature
+++ src/test/resources/Features/AddCustomer.feature
@@ -7,3 +7,44 @@ Feature: Add Customer
     When user need to fill up the fields
     And user click on submit button
     Then user verify customer id is generated
+
+  Scenario: Validate add customer module
+    Given user launches telecom application
+    And user click on add customer button
+    When user need to fill up the fields by one dim list
+      | Java | Selenium | java@gmail.com | Oracle | 8787987987 |
+    And user click on submit button
+    Then user verify customer id is generated
+
+  Scenario: Validate add customer module
+    Given user launches telecom application
+    And user click on add customer button
+    When user need to fill up the fields by one dim map
+      | fname | fayas           |
+      | lname | ahmed           |
+      | mail  | fayas@gmail.com |
+      | addr  | pvi             |
+      | phno  |          545322 |
+    And user click on submit button
+    Then user verify customer id is generated
+
+  Scenario: Validate add customer module
+    Given user launches telecom application
+    And user click on add customer button
+    When user need to fill up the fields by two dim list
+      | Java1 | Selenium  | java@gmail.com | Oracle | 8787987987 |
+      | Java2 | Selenium2 | java@gmail.com | Oracle | 8787987987 |
+      | Java3 | Selenium3 | java@gmail.com | Oracle | 8787987987 |
+    And user click on submit button
+    Then user verify customer id is generated
+
+  Scenario: Validate add customer module
+    Given user launches telecom application
+    And user click on add customer button
+    When user need to fill up the fields by two dim map
+      | FN    | LN        | ML             | AD     | PH         |
+      | Java1 | Selenium  | java@gmail.com | Oracle | 8787987987 |
+      | Java2 | Selenium2 | java@gmail.com | Oracle | 8787987987 |
+      | Java3 | Selenium3 | java@gmail.com | Oracle | 8787987987 |
+    And user click on submit button
+    Then user verify customer id is generated
